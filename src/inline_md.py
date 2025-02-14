434ab3eb3df72e1941c2bb8e5e5307fe8e8d2941
jpsilvadev <jpsjoaosilva98@gmail.com>
2025-02-14 18:45:37 +0000

    handle inline images and links

--- src/inline_md.py
+++ src/inline_md.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 
@@ -30,4 +31,72 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(segments[i], text_type))
 
         nodes.extend(split_nodes)
+    return nodes
+
+
+def extract_markdown_images(text):
+    image_pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    image_matches = re.findall(image_pattern, text)
+    return image_matches
+
+
+def extract_markdown_links(text):
+    link_pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    link_matches = re.findall(link_pattern, text)
+    return link_matches
+
+
+def split_nodes_image(old_nodes):
+    for old_node in old_nodes:
+        image_nodes = extract_markdown_images(old_node.text)
+
+        nodes = []
+
+        # needs to be sliced off each iteration
+        text_to_process = old_node.text
+        for image_node in image_nodes:
+            before, after = text_to_process.split(
+                f"![{image_node[0]}]({image_node[1]})", 1
+            )
+
+            # can use TextNode here to avoid processing downstream
+            nodes.append(TextNode(before, TextType.TEXT))
+
+            # handle the link itself
+            nodes.append(TextNode(image_node[0], TextType.IMAGE, image_node[1]))
+
+            text_to_process = after
+
+        # handle remaining text or return original object if no images were provided
+        if text_to_process:
+            nodes.append(TextNode(text_to_process, TextType.TEXT))
+
+    return nodes
+
+
+def split_nodes_link(old_nodes):
+    for old_node in old_nodes:
+        link_nodes = extract_markdown_links(old_node.text)
+
+        nodes = []
+
+        # needs to be sliced off each iteration
+        text_to_process = old_node.text
+        for link_node in link_nodes:
+            before, after = text_to_process.split(
+                f"[{link_node[0]}]({link_node[1]})", 1
+            )
+
+            # can use TextNode here to avoid processing downstream
+            nodes.append(TextNode(before, TextType.TEXT))
+
+            # handle the link itself
+            nodes.append(TextNode(link_node[0], TextType.LINK, link_node[1]))
+
+            text_to_process = after
+
+        # handle remaining text or return original object if no links were provided
+        if text_to_process:
+            nodes.append(TextNode(text_to_process, TextType.TEXT))
+
     return nodes
--- src/test_inline_md.py
+++ src/test_inline_md.py
@@ -1,6 +1,12 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_md import split_nodes_delimiter
+from inline_md import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link,
+)
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -99,6 +105,161 @@ class TestSplitNodesDelimiter(unittest.TestCase):
                 TextNode("italic multi word", TextType.ITALIC),
                 TextNode(" format", TextType.TEXT),
             ],
+        )
+
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_image_extraction(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(
+            extract_markdown_images(text),
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+        )
+
+    def test_image_extraction2(self):
+        text = "This is text with a ![funny image](https://i.imgur.com/testabc.gif) and ![not fun image](https://i.imgur.com/testxyz.jpeg)"
+        self.assertEqual(
+            extract_markdown_images(text),
+            [
+                ("funny image", "https://i.imgur.com/testabc.gif"),
+                ("not fun image", "https://i.imgur.com/testxyz.jpeg"),
+            ],
+        )
+
+    def test_no_image(self):
+        text = "This is text without images"
+        self.assertEqual(extract_markdown_images(text), [])
+
+
+class TestExtractMarkDownLinks(unittest.TestCase):
+    def test_link_extraction(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(
+            extract_markdown_links(text),
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+        )
+
+    def test_no_link(self):
+        text = "This is text without links"
+        self.assertEqual(extract_markdown_links(text), [])
+
+
+class TestSplitNodesImage(unittest.TestCase):
+    def test_single_image(self):
+        node = TextNode(
+            "This is text and here is an image ![alt text](https://example.com/image.png) followed by more text.",
+            TextType.TEXT,
+        )
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("This is text and here is an image ", TextType.TEXT),
+                TextNode("alt text", TextType.IMAGE, "https://example.com/image.png"),
+                TextNode(" followed by more text.", TextType.TEXT),
+            ],
+        )
+
+    def test_EOL_image(self):
+        node = TextNode(
+            "This is text and here is an image ![alt text](https://example.com/image.png)",
+            TextType.TEXT,
+        )
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("This is text and here is an image ", TextType.TEXT),
+                TextNode("alt text", TextType.IMAGE, "https://example.com/image.png"),
+            ],
+        )
+
+    def test_multiple_images(self):
+        node = TextNode(
+            "This is text and here is an image ![alt text](https://example.com/image.png) followed by another ![alt text second image](https://example.com/image2.png).",
+            TextType.TEXT,
+        )
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("This is text and here is an image ", TextType.TEXT),
+                TextNode("alt text", TextType.IMAGE, "https://example.com/image.png"),
+                TextNode(" followed by another ", TextType.TEXT),
+                TextNode(
+                    "alt text second image",
+                    TextType.IMAGE,
+                    "https://example.com/image2.png",
+                ),
+                TextNode(".", TextType.TEXT),
+            ],
+        )
+
+    def test_no_images(self):
+        # return the original TextNode
+        node = TextNode("This is text without images", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_image([node]),
+            [TextNode("This is text without images", TextType.TEXT)],
+        )
+
+
+class TestSplitNodesLink(unittest.TestCase):
+    def test_single_link(self):
+        node = TextNode(
+            "Here is [a link](https://example.com) and some more text after it.",
+            TextType.TEXT,
+        )
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("Here is ", TextType.TEXT),
+                TextNode("a link", TextType.LINK, "https://example.com"),
+                TextNode(" and some more text after it.", TextType.TEXT),
+            ],
+        )
+
+    def test_EOL_link(self):
+        node = TextNode(
+            "This is text with a link [to example](https://example.com)",
+            TextType.TEXT,
+        )
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to example", TextType.LINK, "https://example.com"),
+            ],
+        )
+
+    def test_multiple_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode(
+                    "to youtube",
+                    TextType.LINK,
+                    "https://www.youtube.com/@bootdotdev",
+                ),
+            ],
+        )
+
+    def test_no_links(self):
+        # return the original TextNode
+        node = TextNode("This is text without links", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_link([node]),
+            [TextNode("This is text without links", TextType.TEXT)],
         )
 
 
