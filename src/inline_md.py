cab338ded8725e4d581e1e594cdaec94a3ea5500
jpsilvadev <jpsjoaosilva98@gmail.com>
2026-07-21 15:52:23 +0100

    convert raw string to typed text nodes

--- src/inline_md.py
+++ src/inline_md.py
@@ -111,3 +111,12 @@ def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
             new_nodes.append(TextNode(original_text, TextType.TEXT))
 
     return new_nodes
+
+
+def text_to_textnodes(text: str) -> list[TextNode]:
+    nodes = split_nodes_image([TextNode(text, TextType.TEXT)])
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    return nodes
--- src/test_inline_md.py
+++ src/test_inline_md.py
@@ -5,7 +5,8 @@ from inline_md import (
     extract_markdown_links,
     split_nodes_delimiter,
     split_nodes_image,
-    split_nodes_link
+    split_nodes_link,
+    text_to_textnodes,
 )
 from textnode import TextNode, TextType
 
@@ -240,9 +241,7 @@ class TestSplitNodesImage(unittest.TestCase):
         self.assertListEqual(
             [
                 TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
-                TextNode(
-                    "second", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
-                ),
+                TextNode("second", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
             ],
             new_nodes,
         )
@@ -408,6 +407,84 @@ class TestSplitNodesLink(unittest.TestCase):
 
     def test_split_links_empty_list(self):
         self.assertListEqual(split_nodes_link([]), [])
+
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_all_inline_types(self):
+        text = (
+            "This is **text** with an _italic_ word and a `code block` and an "
+            "![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a "
+            "[link](https://boot.dev)"
+        )
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            new_nodes,
+        )
+
+    def test_plain_text(self):
+        new_nodes = text_to_textnodes("This is plain text")
+        self.assertListEqual([TextNode("This is plain text", TextType.TEXT)], new_nodes)
+
+    def test_empty_string(self):
+        self.assertListEqual(text_to_textnodes(""), [])
+
+    def test_only_image(self):
+        new_nodes = text_to_textnodes(
+            "![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual(
+            [
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                )
+            ],
+            new_nodes,
+        )
+
+    def test_only_link(self):
+        new_nodes = text_to_textnodes("[link](https://boot.dev)")
+        self.assertListEqual(
+            [TextNode("link", TextType.LINK, "https://boot.dev")], new_nodes
+        )
+
+    def test_url_with_underscore_not_treated_as_italic(self):
+        # regression: delimiter splitting must not run on image/link URLs
+        text = "![alt](https://example.com/my_image.png)"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [TextNode("alt", TextType.IMAGE, "https://example.com/my_image.png")],
+            new_nodes,
+        )
+
+    def test_multiple_bold_and_italic(self):
+        text = "**bold1** normal _italic1_ **bold2** normal _italic2_"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("bold1", TextType.BOLD),
+                TextNode(" normal ", TextType.TEXT),
+                TextNode("italic1", TextType.ITALIC),
+                TextNode(" ", TextType.TEXT),
+                TextNode("bold2", TextType.BOLD),
+                TextNode(" normal ", TextType.TEXT),
+                TextNode("italic2", TextType.ITALIC),
+            ],
+            new_nodes,
+        )
 
 
 if __name__ == "__main__":
